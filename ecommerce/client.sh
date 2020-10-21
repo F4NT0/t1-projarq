8b27e64059b67655fc732519a9692f851e2210c2
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-21 10:23:07 -0300

    old project saved

--- ecommerce/client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ------------------------------------------
-# START AND CREATE A NEW CLIENTE CONNECTION
-# -----------------------------------------
-
-java -cp src/classes/ ecommerce.src.main.java.projarq.cliente.Client
